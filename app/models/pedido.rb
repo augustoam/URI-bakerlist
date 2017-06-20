db2939fb97d526ba66cab5804a609ba78e5aca01
Augusto Pedrollo <augusto@wonder.com.br>
2017-06-20 17:59:43 -0300

    Ajustes gerais

--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -1,6 +1,7 @@
 class Pedido < ApplicationRecord
 	belongs_to :forma_pagamento
 	belongs_to :endereco
+  belongs_to :usuario
 	has_many :itens_pedido, dependent: :destroy, class_name: "ItemPedido"
   	has_many :itens, through: :itens_pedido
 end
