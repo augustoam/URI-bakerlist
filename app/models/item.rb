af31a3a7893eb847c82231b8420f3775662c137d
Augusto Pedrollo <augusto@wonder.com.br>
2017-06-22 08:55:38 -0300

    Ajustes gerais

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,5 @@
 class Item < ApplicationRecord
 	belongs_to :categoria
 	belongs_to :unidade
-	has_many :itens_pedido, dependent: :destroy, class_name: "ItemPedido"
-  	has_many :pedidos, through: :itens_pedido
+	has_many :itens_pedido
 end
--- app/models/pedido.rb
+++ app/models/pedido.rb
@@ -2,6 +2,5 @@ class Pedido < ApplicationRecord
 	belongs_to :forma_pagamento
 	belongs_to :endereco
   belongs_to :usuario
-	has_many :itens_pedido, dependent: :destroy, class_name: "ItemPedido"
-  	has_many :itens, through: :itens_pedido
+	has_many :itens_pedido
 end
